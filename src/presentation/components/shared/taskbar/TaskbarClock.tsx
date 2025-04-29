460514aecffc9eba724ea7d6cd55dfa57e2ae68d
migas <ekmigasari@gmail.com>
2025-04-29 17:34:21 +0700

    feat: buy a coffee

--- src/presentation/components/shared/taskbar/TaskbarClock.tsx
+++ src/presentation/components/shared/taskbar/TaskbarClock.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Coffee } from "lucide-react";
 import React, { useState, useEffect } from "react";
 
 export const TaskbarClock = () => {
@@ -13,8 +14,21 @@ export const TaskbarClock = () => {
   }, []);
 
   return (
-    <div className="taskbar-clock ml-auto mr-2 text-sm font-medium">
-      {time.toLocaleTimeString([], { hour: "numeric", minute: "2-digit" })}
+    <div className="taskbar-clock ml-auto flex items-center gap-2">
+      <div className="flex items-center space-x-1">
+        <a
+          href="https://ko-fi.com/workfromcoffee"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm font-medium hover:underline"
+          title="Buy me a coffee"
+        >
+          <Coffee size={16} />
+        </a>
+      </div>
+      <div className="taskbar-clock ml-auto mr-2 text-sm font-medium">
+        {time.toLocaleTimeString([], { hour: "numeric", minute: "2-digit" })}
+      </div>
     </div>
   );
 };
