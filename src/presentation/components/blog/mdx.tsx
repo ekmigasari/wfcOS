d664247c41ec631cf285f30289e12cc80d7d77f8
migas <ekmigasari@gmail.com>
2025-05-09 15:21:58 +0700

    feat: fix blog error

--- src/presentation/components/blog/mdx.tsx
+++ src/presentation/components/blog/mdx.tsx
@@ -99,13 +99,11 @@ function createHeading(level: number) {
     return React.createElement(
       `h${level}`,
       { id: slug },
-      [
-        React.createElement("a", {
-          href: `#${slug}`,
-          key: `link-${slug}`,
-          className: "anchor",
-        }),
-      ],
+      React.createElement("a", {
+        href: `#${slug}`,
+        key: `link-${slug}`,
+        className: "anchor",
+      }),
       children
     );
   };
