3f714eb610eed4f15a656262ce2aeebd4418f8fd
migas <ekmigasari@gmail.com>
2025-05-17 20:33:59 +0700

    wip: setup payment with polar

--- src/infrastructure/lib/auth-client.ts
+++ src/infrastructure/lib/auth-client.ts
@@ -1,8 +1,10 @@
 import { createAuthClient } from "better-auth/react";
+import { polarClient } from "@polar-sh/better-auth";
 
 const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  plugins: [polarClient()],
 });
 
 export const { signIn, signUp, signOut, useSession, getSession, updateUser } =
--- src/infrastructure/utils/auth.ts
+++ src/infrastructure/utils/auth.ts
@@ -1,7 +1,14 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-
+import {
+  polar,
+  checkout,
+  portal,
+  usage,
+  // webhooks,
+} from "@polar-sh/better-auth";
 import { prisma } from "./prisma";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -13,4 +20,31 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            // {
+            //     productId: "123-456-789", // ID of Product from Polar Dashboard
+            //     slug: "pro" // Custom slug for easy reference in Checkout URL, e.g. /checkout/pro
+            // }
+          ],
+          successUrl: "/success?checkout_id={CHECKOUT_ID}",
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+        usage(),
+        // webhooks({
+        //     secret: process.env.POLAR_WEBHOOK_SECRET,
+        //     onCustomerStateChanged: (payload) => // Triggered when anything regarding a customer changes
+        //     onOrderPaid: (payload) => // Triggered when an order was paid (purchase, subscription renewal, etc.)
+        //     ...  // Over 25 granular webhook handlers
+        //     onPayload: (payload) => // Catch-all for all events
+        // })
+      ],
+    }),
+  ],
 });
