6e88ffe5a1cca6b8f19c4a6d8c48595848f8715c
ekmigasari <ekmigasari@gmail.com>
2025-05-21 13:42:19 +0700

    wip: subscriptions schema

--- src/infrastructure/utils/polar.ts
+++ src/infrastructure/utils/polar.ts
@@ -2,8 +2,6 @@ import { Polar } from "@polar-sh/sdk";
 
 export const polarClient = new Polar({
   accessToken: process.env.POLAR_ACCESS_TOKEN,
-  // Use 'sandbox' if you're using the Polar Sandbox environment
-  // Remember that access tokens, products, etc. are completely separated between environments.
-  // Access tokens obtained in Production are for instance not usable in the Sandbox environment.
-  server: "sandbox",
+  server:
+    process.env.NEXT_PUBLIC_ENV === "production" ? "production" : "sandbox",
 });
